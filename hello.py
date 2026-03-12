6f2a7f9b073a9f515249b063379c064a26fd3988
F project <100823083+fereysitnerya@users.noreply.github.com>
2026-03-12 20:47:30 +0200

    ci: decor

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello, world!")
+def shout(func):
+    def wrapper():
+        print(">>> старт функции")
+        result = func()
+        print(">>> конец функции")
+        return result
+    return wrapper
+
+
+@shout
+def hello():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    hello()
